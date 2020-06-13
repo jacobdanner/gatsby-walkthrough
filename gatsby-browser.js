57b3b7fd05922c04117ce39ded47bd026f524e4c
Jacob Danner <jacob.danner@gmail.com>
2020-06-13 11:46:21 -0700

    Completed Styling tutorial using inline components and css modules

---
+++ gatsby-browser.js
@@ -0,0 +1,2 @@
+import "./src/styles/global.css" // ES Modules
+// or: require('./src/styles/global.css') // CommonJS , required for Node.js environment
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
+import "../styles/global.css" // ES Modules
+
 
 export default function Home() {
   // In pure JS - return React.createElement("div", null, "Hello world!")
@@ -8,6 +10,6 @@ export default function Home() {
     <Link to="/contact/">Contact</Link>
     <Header headerText="Hello Gatsby!" />
     <p>What a world!</p>
-    <img src="https://source.unsplash.com/random/400x200" alt="Random Image"/>
+    <img src="https://source.unsplash.com/random/400x200" alt="Random"/>
     </div>
 }
