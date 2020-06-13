bd3f13c238429cc2eaf319f30dd5f06739855513
Jacob Danner <jacob.danner@gmail.com>
2020-06-13 15:10:35 -0700

    fix up remaining issues

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,6 @@
 import React from "react"
-import {Link} from "gatsby"
-import Header from "../components/header"
+// import {Link} from "gatsby"
+// import Header from "../components/header"
 import Layout from "../components/layout"
 
 export default function Contact() {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 import Header from "../components/header"
 import "../styles/global.css" // ES Modules
 import Layout from "../components/layout"
@@ -17,5 +17,12 @@ export default function Home() {
         websites.
       </p>
     </div>
+    <h1>Amazing Pandas Eating Things</h1>
+      <div>
+        <img
+          src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
+          alt="Group of pandas eating bamboo"
+        />
+      </div>
     </Layout>
 }
