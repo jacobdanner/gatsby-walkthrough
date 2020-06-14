453ae73a3a9c8a332e177f19627de9238058a34b
Jacob Danner <jacob.danner@gmail.com>
2020-06-14 10:22:23 -0700

    added SEO and additional plugins for auditing and pre-publish verification

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,9 @@
 
 module.exports = {
   siteMetadata:{
-    title: "Pandas Eating"
+    title: "Pandas Eating",
+    description: `A simple description about pandas eating lots...`,
+    author: `gatsbyjs`,
   },
   plugins: [
     {
@@ -24,6 +26,24 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // The offline plugin should be listed after the manifest plugin so that the offline plugin can cache the created manifest.webmanifest
+    `gatsby-plugin-offline`,
+    //  React component interface for you to manage your document head
+    `gatsby-plugin-react-helmet`,
 
 
   ],
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 // This is the REACT conmponent to use to render markdown pages
 // see /gatsby-node.js createPages
@@ -8,6 +9,7 @@ export default function BlogPost( {data} ) {
     const post = data.markdownRemark
     return (
         <Layout>
+            <SEO title={post.frontmatter.title} description={post.excerpt} />
             <h1>{post.frontmatter.title}</h1>
 
             <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
@@ -22,6 +24,7 @@ export const query = graphql`
       frontmatter {
         title
       }
+      excerpt
     }
   }
 `
