ba1aaad0f11be9eeb728d3fd0cfb4812614240d7
Jacob Danner <jacob.danner@gmail.com>
2020-06-13 21:17:37 -0700

    part 6, use md transformer and graphql over files to update index.js with pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import React from "react"
-// import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
+import { css } from "@emotion/core"
+import { rhythm } from "../utils/typography"
 import Header from "../components/header"
 import "../styles/global.css" // ES Modules
 import Layout from "../components/layout"
 
-export default function Home() {
+export default function Home({data}) {
+  console.log(data);
   // In pure JS - return React.createElement("div", null, "Hello world!")
   return <Layout>
     <Header headerText="Hello Gatsby!" />
@@ -24,5 +27,51 @@ export default function Home() {
           alt="Group of pandas eating bamboo"
         />
       </div>
+      <div>
+        <h1 css={css`
+          display: inline-block;
+          border-bottom: 1px solid;
+        `}>Amazing Pandas Eating Things</h1>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+        {data.allMarkdownRemark.edges.map(( {node}, index) =>(
+       <div key={node.id}>
+       <h3
+         css={css`
+           margin-bottom: ${rhythm(1 / 4)};
+         `}
+       >
+         {node.frontmatter.title}{" "}
+         <span
+           css={css`
+             color: #bbb;
+           `}
+         >
+           — {node.frontmatter.date}
+         </span>
+       </h3>
+       <p>{node.excerpt}</p>
+     </div>
+   ))}
+ </div>
     </Layout>
 }
+
+export const query = graphql`
+  query{
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }){
+    totalCount
+    edges {
+      node {
+        id
+        frontmatter{
+          title
+          date(formatString: "DD MMMM, YYYY")
+        }
+        excerpt
+        timeToRead
+        html
+      }
+    }
+  }
+}
+`
